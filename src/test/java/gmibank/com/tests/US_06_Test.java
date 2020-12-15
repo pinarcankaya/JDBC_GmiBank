d4e350fa806d3f7d6056d2aa343355d36f81043a
pinarcankaya <pinarunalcan50@gmail.com>
2020-12-15 23:08:45 +0100

    US_06 15-12-2020

--- src/test/java/gmibank/com/tests/US_06_Test.java
+++ src/test/java/gmibank/com/tests/US_06_Test.java
@@ -13,62 +13,66 @@ public class US_06_Test {
 
 
     //1--Asert that the city of Athens was written 10 times
-    //(Atina'nin 10 kez yazildigini asert eder)
-
+    //(Atina'nin 7 kez yazildigini asert eder)
     @Test
     public void TC_0601() throws SQLException {
-
-        String queryAthens="select count(city)\n" +
+        String queryAthens="select count(city) as state_count\n" +
                 "from tp_customer\n" +
-                "where city='Athens'" ;
-        List<Map<String,String>> resultList= DatabaseConnector.getQueryAsAListOfMaps(queryAthens);
-        System.out.println(resultList);
+                "where city='Athens'";
 
-       Assert.assertEquals(resultList.get(0).get("count"),"7");
+        List<Map<String,String>> resultList=DatabaseConnector.getQueryAsAListOfMaps(queryAthens);
+        System.out.println(resultList);
+        Assert.assertEquals(resultList.get(0).get("state_count"),"7");
+
+
     }
 
     //2---After sorting by name, get the smallest count
     //(Isme göre sıraladıktan sonra en küçük sayıyı(count'u) alıniz)
+    //en kucuk sayinin 888 oldugunu dogrulayiniz//
     @Test
     public void TC_0602() throws SQLException {
-        String queryMin="select  min(event_id),name\n" +
-                "from  jhi_persistent_audit_evt_data\n" +
+        String queryName="select count (name) as names, name\n" +
+                "from jhi_persistent_audit_evt_data\n" +
                 "group by name\n" +
-                "order by name limit 1 ";
-        List<Map<String,String>> nameList=DatabaseConnector.getQueryAsAListOfMaps(queryMin);
+                "order by names asc limit 1";
+
+        List<Map<String,String>> nameList=DatabaseConnector.getQueryAsAListOfMaps(queryName);
         System.out.println(nameList);
-        Assert.assertEquals(nameList.get(0).get("min"),"10450");
+        Assert.assertEquals(nameList.get(0).get("names"),"888");
+
 
     }
 
+    //Find out which state's total number is more than 10
     //3---(hangi eyaletin toplam sayisi 10'dan fazla ise bulunuz)
+    //toplam sayisi 10'dan fazla olan 4 ulke oldugunu dogrulayiniz
+    //toplam sayisi 10'dan fazla olan 4.ulkenin "Koblenz" oldugunu dogrulayiniz
     @Test
-    public void TC_0603() throws SQLException, UnsupportedEncodingException {
-        String stateQuery="select name,count (name)\n" +
+    public void TC_0603() throws SQLException {
+        String stateQuery="select name,count(name)\n" +
                 "from tp_state\n" +
-                "group  by name\n" +
-                "having count(name)>10" ;
-        List<Map<String,String>> stateList= DatabaseConnector.getQueryAsAListOfMaps(stateQuery);
+                "group by name\n" +
+                "having count(name)>10";
+
+        List<Map<String,String>> stateList=DatabaseConnector.getQueryAsAListOfMaps(stateQuery);
         System.out.println(stateList);
+
         System.out.println(stateList.size());
         Assert.assertEquals(stateList.size(),4);
 
         System.out.println(stateList.get(3).get("name"));
         Assert.assertEquals(stateList.get(3).get("name"),"Koblenz");
 
-        for(Map<String,String> w: stateList){
+        for (Map<String,String> w:stateList){
             if(w.containsValue("Koblenz")){
                 Assert.assertEquals(w.get("name"),"Koblenz");
                 break;
             }
         }
-//
-//        boolean isExist = false;
-//        for (Map<String,String> w:stateList){
-//            if(w.containsValue("Koblenz")) {
-//                isExist = true;
-//            }
 
-        }
-     //   Assert.assertTrue(isExist);   }
+
+
+
+    }
 }
