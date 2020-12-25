329f09db48083056dd3a518e7e5804a28bfee346
zehraosmanogullari <zehraozcelik14@gmail.com>
2020-12-25 09:02:23 +0100

    m

---
+++ src/test/java/gmibank/com/tests/US_01_Test.java
@@ -0,0 +1,12 @@
+package gmibank.com.tests;
+
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+
+public class US_01_Test {
+    ResultSet resultSet;
+    List<Map<String,String>> list;
+    String query_tc11_01 = "";
+
+}
